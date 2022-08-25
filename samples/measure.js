c82f93a39c2e9fef8e71fb35accd4a27a248d33c
Alexandre Capt <acapt@adobe.com>
2022-08-25 12:04:05 +0200

    feat: use adjust query param

--- samples/measure.js
+++ samples/measure.js
@@ -2,16 +2,16 @@ let cls = 0;
 new PerformanceObserver((entryList) => {
   for (const entry of entryList.getEntries()) {
     // 500 ms input exclusion window
-    if (!entry.hadRecentInput) {
-      cls += entry.value;
-      console.log('CONSOLE - Current CLS value:', cls, entry);
-    }
+    if (entry.hadRecentInput) cls = 0;
+    cls += entry.value;
+    console.log('CONSOLE - Current CLS value:', cls, entry);
   }
 // the buffered flag enables observer to access entries from before the observer creation
 }).observe({type: 'layout-shift', buffered: false});
 
 window.setInterval(() => {
   let c = document.querySelector('.cls');
+  const displayCLS = Math.round(cls * 10000) / 1000;
   if (!c) {
     c = document.createElement('div');
     c.className = 'cls';
@@ -19,13 +19,15 @@ window.setInterval(() => {
     c.style.top = '50';
     c.style.left = '50';
     c.style['z-index'] = '9999';
-    if (cls > 0.0001) {
+    if (displayCLS > 0.25) {
       c.style['background-color'] = 'red';
+    } else if (displayCLS > 0.1) {
+      c.style['background-color'] = 'orange';
     } else {
       c.style['background-color'] = 'lightgreen';
     }
     c.style['padding'] = '30px';
     document.body.appendChild(c);
   }
-  c.innerHTML = `Current CLS value: ${cls}`;
-}, 3000);
+  c.innerHTML = `Current CLS value: ${displayCLS}`;
+}, 2000);
