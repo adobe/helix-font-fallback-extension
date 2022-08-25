3d5035aea91bae37f50af6e087b6692bfd840623
Alexandre Capt <acapt@adobe.com>
2022-08-25 15:57:18 +0200

    feat: show cls box

--- tools/simulator/measure.js
+++ tools/simulator/measure.js
@@ -15,26 +15,24 @@ new PerformanceObserver((entryList) => {
 }).observe({type: 'layout-shift', buffered: false});
 
 window.setInterval(() => {
-  if (window.markers !== 'no') {
-    let c = document.querySelector('.cls');
-    const displayCLS = Math.round(cls * 10000) / 1000;
-    if (!c) {
-      c = document.createElement('div');
-      c.className = 'cls';
-      c.style.position = 'absolute';
-      c.style.top = '50';
-      c.style.left = '50';
-      c.style['z-index'] = '9999';
-      if (displayCLS > 0.25) {
-        c.style['background-color'] = 'red';
-      } else if (displayCLS > 0.1) {
-        c.style['background-color'] = 'orange';
-      } else {
-        c.style['background-color'] = 'lightgreen';
-      }
-      c.style['padding'] = '30px';
-      document.body.appendChild(c);
+  let c = document.querySelector('.cls');
+  const displayCLS = Math.round(cls * 10000) / 1000;
+  if (!c) {
+    c = document.createElement('div');
+    c.className = 'cls';
+    c.style.position = 'absolute';
+    c.style.top = '50';
+    c.style.left = '50';
+    c.style['z-index'] = '9999';
+    if (displayCLS > 0.25) {
+      c.style['background-color'] = 'red';
+    } else if (displayCLS > 0.1) {
+      c.style['background-color'] = 'orange';
+    } else {
+      c.style['background-color'] = 'lightgreen';
     }
-    c.innerHTML = `Current CLS value: ${displayCLS}`;
+    c.style['padding'] = '30px';
+    document.body.appendChild(c);
   }
+  c.innerHTML = `Current CLS value: ${displayCLS}`;
 }, 2000);
