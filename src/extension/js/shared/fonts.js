ffdc236eb261a8ab8f825a5b0f58185d355db68d
Tobias Bocanegra <tripod@adobe.com>
2025-08-12 15:52:04 +0200

    chore: refactor to new eslint

--- src/extension/js/shared/fonts.js
+++ src/extension/js/shared/fonts.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
+/* eslint-disable no-param-reassign */
 const WEIGHT_MAPPING = {
   thin: 100,
   'extra-light': 200,
